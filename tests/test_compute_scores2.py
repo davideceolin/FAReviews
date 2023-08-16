d63e2eee7146c953fe7b21b459a2869904eea625
LAPTOP-8R728B5R\guusl <l.ootes@esciencecenter.nl>
2023-08-16 17:16:24 +0200

    correct setup for using nlp pipe in tests

--- tests/test_compute_scores2.py
+++ tests/test_compute_scores2.py
@@ -1,27 +1,21 @@
-"""Unit tests for the file compute_score2.py."""
 import unittest
-import spacy
-from spacy.language import Language
-from spacy_readability import Readability
-from compute_scores2 import apply_ranking # noqa: F401
-
-@Language.component("readability")
-def readability(doc):
-    read = Readability()
-    doc = read(doc)
-    return doc
-
-nlp = spacy.load('en_core_web_md')
-nlp.add_pipe("textrank", last=True)
-nlp.add_pipe("readability", last=True)
-
+from unittest.mock import patch, Mock
 
 class TestComputeScores2(unittest.TestCase):
-    """Unit tests for compute_scores2.
+    def mock_spacy_pipeline(self):
+        mock_nlp = Mock()
 
-    This class contains the following functions:
-    -
-    """
+        # Define a mock component
+        class MockComponent:
+            def _init_(self):
+                pass
 
-    def test_test(self):
-        print('hello tests')
+        # Add the mock component to the mock nlp object
+        mock_nlp.add_pipe.return_value = MockComponent()
+
+        return mock_nlp
+
+    @patch('spacy.load', side_effect=mock_spacy_pipeline)  # Mock the spacy.load function
+    def test_apply_ranking(self, mock_load):
+        from compute_scores2 import apply_ranking # noqa: F401
+        print('test')
