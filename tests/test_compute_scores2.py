004a4c2488ced6e05cc90269d17f9c4ecbda3c6d
Laura Ootes <l.ootes@esciencecenter.nl>
2023-08-16 11:43:23 +0200

    fix flake8 errors

--- tests/test_compute_scores2.py
+++ tests/test_compute_scores2.py
@@ -1,13 +1,13 @@
 """Unit tests for the file compute_score2.py."""
 import unittest
-import pandas as pd
-import compute_scores2
+import compute_scores2  # noqa: F401
+
 
 class TestComputeScores2(unittest.TestCase):
     """Unit tests for compute_scores2.
 
     This class contains the following functions:
-    - 
+    -
     """
 
     def test_test(self):
