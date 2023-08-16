0d3bccaef5515e579cc935a831d44d0445721816
Laura Ootes <l.ootes@esciencecenter.nl>
2023-08-16 11:41:04 +0200

    initial commit

---
+++ tests/test_compute_scores2.py
@@ -0,0 +1,14 @@
+"""Unit tests for the file compute_score2.py."""
+import unittest
+import pandas as pd
+import compute_scores2
+
+class TestComputeScores2(unittest.TestCase):
+    """Unit tests for compute_scores2.
+
+    This class contains the following functions:
+    - 
+    """
+
+    def test_test(self):
+        print('hello tests')
