b05d00fa227e73219736fa6d5d09e0a49f4d40bb
Laura Ootes <l.ootes@esciencecenter.nl>
2023-08-16 13:57:51 +0200

    add pipeline to test

--- tests/test_compute_scores2.py
+++ tests/test_compute_scores2.py
@@ -1,6 +1,19 @@
 """Unit tests for the file compute_score2.py."""
 import unittest
-import compute_scores2  # noqa: F401
+import spacy
+from spacy.language import Language
+from spacy_readability import Readability
+from compute_scores2 import apply_ranking # noqa: F401
+
+@Language.component("readability")
+def readability(doc):
+    read = Readability()
+    doc = read(doc)
+    return doc
+
+nlp = spacy.load('en_core_web_md')
+nlp.add_pipe("textrank", last=True)
+nlp.add_pipe("readability", last=True)
 
 
 class TestComputeScores2(unittest.TestCase):
